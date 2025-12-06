73446e0f5b892a3df30766067fcc56c280abdc04
Webmaker669 <ethynh2010@gmail.com>
2025-12-06 14:29:35 -0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Webmaker669/Code/refs/heads/main/Code.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Webmaker669/Code/refs/heads/main/Code.lua?token=GHSAT0AAAAAADP2WFWKFWFOLHMLWZZZDUIO2JUQQ2Q"))()
